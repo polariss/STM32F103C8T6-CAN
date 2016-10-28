d38bb6e0a5c764365ed79bdf75feb4bc2e434b89
Polaris <mailmalongfei@163.com>
2016-10-28 15:20:17 +0800

    del main function

--- User/main.c
+++ User/main.c
@@ -3,35 +3,7 @@
 
 void LED_Display(uint8_t Ledstatus);
 //注释测试
-int main(void)
-{
-	uint8_t temp = 0x30;
- 	SystemInit();//源自system_stm32f10x.c文件,只需要调用此函数,则可完成RCC的配置.具体请看2_RCC
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
-	
-	USART1_Init();
-	LED_Init();
-	
-#ifdef	LOOPBACK_TEST
-	printf("CAN test start!\n");
-	CAN_LoopbackTest();
-	xxoo(1);
-#else
-	CAN_Config();
-#endif /*LOOPBACK_TEST*/
-	
-	
-	printf("CAN demo start!\n");
-	xxoo(1)
-	{
-//		LED_RED_TOGGLE;
-//		LED_GREEN_TOGGLE;
-//		LED_BLUE_TOGGLE;
-		CAN_SendBytes( &temp, 1);
-		temp++;
-		delay_ms(1000);
-	}
-}
+
 
 /**
   * @brief  Turn ON/OFF the dedicate led
